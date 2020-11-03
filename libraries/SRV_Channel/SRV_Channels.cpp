67a3e78bcf1675525fe7a86f6b87bfd3a384e2d8
hmedehuss <67143928+hmedehuss@users.noreply.github.com>
2020-11-03 16:37:07 +0100

    stash

--- libraries/SRV_Channel/SRV_Channels.cpp
+++ libraries/SRV_Channel/SRV_Channels.cpp
@@ -380,8 +380,11 @@ void SRV_Channels::push()
     }
 #endif // HAL_NUM_CAN_IFACES
 }
+<<<<<<< HEAD
 
 void SRV_Channels::set_AutoMode(bool in_modeauto)
 {
     in_auto = in_modeauto;
 }
+=======
+>>>>>>> parent of b4705b142b... Merge remote-tracking branch 'upstream/master' into Viable
