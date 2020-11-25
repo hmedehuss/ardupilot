53bf6b505319432e2466da8e180c99229e0e5b9d
hmedehuss <husseinhamadi.02041993@gmail.com>
2020-11-25 12:08:15 +0100

    FlightGear

--- Tools/autotest/fg_plane_view.sh
+++ Tools/autotest/fg_plane_view.sh
@@ -5,7 +5,7 @@ AUTOTESTDIR=$(dirname $0)
 nice fgfs \
     --native-fdm=socket,in,10,,5503,udp \
     --fdm=external \
-    --aircraft=c172p \
+    --aircraft=Rascal110-JSBSim \
     --fg-aircraft="$AUTOTESTDIR/aircraft" \
     --airport=YKRY \
     --geometry=650x550 \
